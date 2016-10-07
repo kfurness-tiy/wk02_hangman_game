f7407092c3d035476582c28ae438e30069e2a652
kelly <kelly.n.waite@gmail.com>
2016-10-07 15:05:27 -0600

    feat(init) Put array in main.js in new js file
    Easier to read code in main.js without array in way

---
+++ css/common-words.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Here are the 100 most popular words in English, as totally
+// stolen from here: https://gist.github.com/gravitymonkey/2406023
+
+var commonWords = [
+  "the","and","you","that",
+  "was","for","are","with","his","they",
+  "this","have","from","or","one","had","by","word","but",
+  "not",
+  "what","all","were","when","your","can","said","there",
+  "use","each","which","she","do","how","their","will",
+  "other","about","out","many","then","them","these",
+  "some","her","would","make","like","him","into","time",
+  "has",
+  "look","two","more","write","see","number","way",
+  "could","people","than","first","water","been","call",
+  "who","oil","its","now","find","long","down","day","did","get",
+  "come","made","may","part"
+];
