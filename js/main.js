6739569d5e3c8c3d9dd6e8a866d233db9e511bea
kelly <kelly.n.waite@gmail.com>
2016-10-10 20:44:53 -0600

    feat(core) Start over; Add starting functions
    Add on page load to find random word and create/output the
      tracker.
    Add what happens when a player puts in input and that
      letter is correct.
    
    Things kept: Random Word Generator, Beginning of userGuess()
      and playerInput()

--- js/main.js
+++ js/main.js
@@ -1,134 +1,68 @@
 "use strict";
 
-var randomWord = findRandomWord(); // chosen in findRandomWord
-var correctArr = createArray();
-var mistakesLeft = 8;
-var bodyCount = 0;
-var splitRandomWord = randomWord.split("");
-var tracker = "";
-var newTracker = "";
-// var correctGuessArr = []; //This is trying to save correct answers in an array
+let guess;
+let randomWord;
+let tracker= [];
 
-/*create function getRandomNumber to
- help choose randomWord */
+
+// var randomWord = findRandomWord(); // chosen in findRandomWord
+
+    /*create function getRandomNumber to
+     help choose randomWord */
+
 function getRandomNumber (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
+    // choose a random word from array commonWords
 
-function trackerCreator (randomWord) {
-  var randomLength = randomWord.length;
-  tracker = "";
-  for (var i=0; i < randomLength; i++) {
-    tracker += "_";
-  }
-}
-
-function showTracker (tracker) {
-    document.querySelector('.tracker').innerHTML = tracker;
-}
-
-
-// choose a random word from array commonWords
 function findRandomWord () {
   let number = getRandomNumber(0, commonWords.length);
   randomWord = commonWords[number];
-  trackerCreator(randomWord);
-  showTracker(tracker);
   console.log("Random Word: " + randomWord);
   return randomWord;
 }
 
-function createArray (arr) {
-  var arr = [];
-  for (var i=0; i < randomWord.length; i++) {
-    var total = arr.push(i);
+    //Creates the tracker
+function makeTracker (randowWord) {
+  tracker = [];
+  for (var i = 0; i < randomWord.length; i++) {
+    tracker.push(' _');
   }
-  console.log(arr);
-  return arr;
+  return tracker;
 }
 
-/* *************WORK IN PROGRES **************** */
-/* *************WORK IN PROGRES **************** */
-/* *************WORK IN PROGRES **************** */
-/* *************WORK IN PROGRES **************** */
-function correctLetter(guess) {
-  console.log("test: " + correctArr);
-  var correctGuessArr = correctArr;
-  console.log('correctGuessArr: ' + correctArr);
-  var correctSpot = splitRandomWord.indexOf(guess);
-  console.log(correctSpot);
-  correctArr.splice(correctSpot, 1, guess);
-  console.log('final ' + correctArr);
-  console.log(correctArr.join(''));
-}
-
-function showMistake(mistakesMessage) {
-  document.querySelector('.mistakesLeft').innerHTML = mistakesMessage;
-}
-
-function mistakeTracker() {
-  mistakesLeft -= 1;
-  let body = ['head', 'neck', 'arm1', 'arm2', 'torso', 'leg1', 'leg2'];
-  var selectBody = body[bodyCount];
-  if (mistakesLeft > 0) {
-    let mistakesMessage =  "Mistakes Left: " + mistakesLeft;
-    showMistake(mistakesMessage);
-    document.getElementById(selectBody).style.visibility = "visible";
-    bodyCount += 1;
-    console.log(bodyCount);
-  } else {
-    let lostMessage = "I'm sorry to say that you lost. The word was: " + randomWord;
-    document.querySelector('.mistakesLeft').innerHTML = lostMessage;
+function checkGuess (guess, randomWord, tracker) {
+  for (var i = 0; i < randomWord.length; i++) {
+    if (guess === randomWord[i]) {
+      tracker[i] = guess;
+    }
   }
+  console.log(tracker);
+  return tracker;
 }
 
-    /* Shows feedback to player */
-function showResponse(response) {
-  document.querySelector('.response').innerHTML = response;
-}
-
-/*Step 1-b Checking guess */
-function checkGuess(guess) {
-let numberString = "0123456789"
-guess = guess.toLowerCase();
-if (numberString.indexOf(guess) >= 0) {
-return 'Please put in a letter';
-}
-else if (guess.length > 1) {
-return 'Please only type in one letter at a time.'
-}
-else if (splitRandomWord.indexOf(guess) === -1) {
-  mistakeTracker();
-  return "Wrong letter, try again."
-}
-else {
-// var correctGuess = guess;
-correctLetter(guess);
-return "Good job!";
-}
-}
-
-    /* Step 1-a Will put input into var guess
-    (from startGuessCheck()) */
 function playerInput () {
   return document.querySelector('input').value;
 }
 
-    /* Step 1: The function playerInput, gets put inside the checkGuess function (which will be give var guess) */
-function startGuessCheck() {
-  var guess = playerInput(); //Takes input, calls it guess
-  var response = checkGuess(guess);
-    /* checkGuess will return a response based  on the answer
-    and that feedback provided will go to this function and
-    print on the HTML */
-  showResponse(response);
+    //Start game
+
+function userGuess() {
+  guess = playerInput(); //Takes input, calls it guess
+  let check = checkGuess(guess, randomWord, tracker);
+  // showResponse(response);
   return false;//bypass form default
 }
 
+function windowOnload () {
+  findRandomWord();
+  makeTracker(randomWord);
+  document.querySelector('.tracker').innerHTML = tracker.join(' ');
+}
 
-    /* Step ONE: when button is clicked,
-    go to the function byPassDefault */
-document.querySelector('button').onclick = startGuessCheck;
+window.onload = windowOnload();
+
+document.querySelector('button').onclick = userGuess;
